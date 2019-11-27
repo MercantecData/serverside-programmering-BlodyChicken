63c77e3ae69e4fdcb52aaaf84477db75e4c9222f
Ian Fanefjord <i.fanefjord@gmail.com>
2019-11-27 12:13:23 +0100

    Mearge from finished assignment

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Af Ian Fanefjord
 
 Dette repository indeholder elevens svar på de opgaver der er givet på https://www.learnspace.dk/course/view.php?id=2632
+
+Finished Assignments:
+---------------------
+
+Serverside Programmereing:
+- intro (Ny Server med Mysql samt fejlkoder)
+- WWW (Webserver)
--- restAPI/serverAPI.js
+++ restAPI/serverAPI.js
@@ -1,10 +1,15 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var querystring = require('querystring');
 var getPostData = {};
+var path;
+var serverPath;
+var validKeyCode = 123;
 
 var sqlCon = require("./sqlCon.js");
 
+// ******************************** Opret test data *****************************************
 var testData = (req,res,con,db) =>
 {
     var data =
@@ -21,10 +26,10 @@ var testData = (req,res,con,db) =>
         reservation:
             [
                 { LokaleId: 1, FraDato: "STR_TO_DATE('2019-10-15 10:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-10-15 11:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Ole'" },
-                { LokaleId: 2, FraDato: "STR_TO_DATE('2019-11-14 12:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-10-15 13:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Ib'" },
-                { LokaleId: 1, FraDato: "STR_TO_DATE('2019-12-13 08:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-10-15 09:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Mads'" },
-                { LokaleId: 3, FraDato: "STR_TO_DATE('2019-11-14 14:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-10-15 15:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Jakup'" },
-                { LokaleId: 6, FraDato: "STR_TO_DATE('2019-12-24 18:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-10-15 19:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Henrik'" },
+                { LokaleId: 2, FraDato: "STR_TO_DATE('2019-11-14 12:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-11-15 13:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Ib'" },
+                { LokaleId: 1, FraDato: "STR_TO_DATE('2019-12-13 08:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-12-13 09:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Mads'" },
+                { LokaleId: 3, FraDato: "STR_TO_DATE('2019-11-14 14:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-11-14 15:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Jakup'" },
+                { LokaleId: 6, FraDato: "STR_TO_DATE('2019-12-24 18:00:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-12-24 19:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Henrik'" },
                 { LokaleId: 1, FraDato: "STR_TO_DATE('2019-11-25 11:30:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2019-11-25 12:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Ian'" },
                 { LokaleId: 4, FraDato: "STR_TO_DATE('2020-07-06 13:30:00','%Y-%m-%d %H:%i:%s')", TilDato: "STR_TO_DATE('2020-07-06 14:00:00','%Y-%m-%d %H:%i:%s')", Ansvarlig: "'Julemand'" }
             ]
@@ -47,17 +52,14 @@ var testData = (req,res,con,db) =>
             con.query("INSERT INTO " + db + "." + table + " (" + columbNames + ") VALUES(" + columbData + ")", function (err, data) { });
         }
     }
+
     handleRequest(req, res, con);
 }
 
+// ********************** Request/url Styringsmetode ******************
 var handleRequest = (req, res, con) =>
 {
-    
-    var path = url.parse(req.url, true);
-    var serverPath = "." + path.pathname;
-    //var filename = serverPath.split("/")[serverPath.split("/").length - 1].split("?")[0];
-    //var query = path.query;
-    
+    // ********************** Viser oversig over Lokaler ****************
     if (req.url.split("?")[0] == "/rooms")
         con.query("SELECT * FROM booking.rooms", function (err, data) {
             res.write(JSON.stringify(data, null, '\t'));
@@ -71,7 +73,8 @@ var handleRequest = (req, res, con) =>
             // booking?month=11 viser booking samme dag bare i november.  
             // booking?Year=2019 viser booking samme dag og samme måned i 2019.
             // booking?day=06&month=07&year=2020 viser booking den 06-07-2020.
-            var get = req.url.split("?")[1].split("&");
+            
+            var get = "";
             var day = "";
             var month = "";
             var year = "";
@@ -79,8 +82,10 @@ var handleRequest = (req, res, con) =>
             var date = new Date(Date.now());
             var today = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
             var showdate = "";
-            
-            if (get.length > 0)
+
+            if (req.url.indexOf("?") > 0) get = req.url.split("?")[1].split("&");
+
+            if (get.length>0)
             {
                 get.forEach(e => {
                     if (e.split("=")[0] == "day") day = e.split("=")[1];
@@ -95,38 +100,44 @@ var handleRequest = (req, res, con) =>
             }
             else
                 showdate = today;
-            con.query("SELECT * FROM booking.reservation WHERE DATE(FraDato)>='" + showdate + "' AND DATE(TilDato)<='" + showdate + "'", function (err, data) {
+                con.query("SELECT * FROM booking.reservation WHERE DATE(FraDato)>='" + showdate + "' AND DATE(TilDato)<='" + showdate + "'", function (err, data) {
                 res.write(JSON.stringify(data, null, '\t'));
                 res.end();
             });
+            
         }
         else
+            // ************************* Ved POST/bookning af lokale ***********************
             if (req.url.split("?")[0] == "/add")
             {
-                post(req, res,con);
+                bookLokale(req, res,con);
             }
             else
             {
+                // ************************* Vis default index side ***********************
+                if(serverPath="./") serverPath+="index.html";
                 // get file and send to client
                 fs.readFile(serverPath, function (err, data) {
                     // no file found send error to client
                     if (err != null) data = path.pathname + "\n404 Not Found";
+                    res.setHeader("content-type","text/html");
                     res.end(data);
                 });
             }
 }
 
-function post(req, res, con)
+function bookLokale(req, res, con)
 {
     var LokaleId = "";
     var FraDato = "";
     var TilDato = "";
-    var FraTime = "xx";
+    var FraTime = "";
     var TilTime = "";
     var Ansvarlig = "";
 
         if (getPostData.length>0)
         {
+            // løb post elementer igennem og tildel værdier til variable
             getPostData.forEach(e => {
                 if (e.split("=")[0] == "LokaleId") LokaleId = e.split("=")[1];
                 if (e.split("=")[0] == "FraDato") FraDato = e.split("=")[1];
@@ -136,26 +147,65 @@ function post(req, res, con)
                 if (e.split("=")[0] == "Ansvarlig") Ansvarlig = e.split("=")[1];
             });
 
+            
             var fra = FraDato + " " + FraTime + ":00";
             var til = TilDato + " " + TilTime + ":00";
-            var query = "SELECT * FROM booking.reservation WHERE ";
-            query += "FraDato<='" + fra + "' AND TilDato>='" + fra + "'";
-            con.query(query, function (err, data) {
-                res.write(JSON.stringify(data, null, '\t') + "\n");
-                res.end("POST OK!" + JSON.stringify(fra, null, '\t'));
+            
+            var query = "SELECT * FROM booking.rooms WHERE PNR='" + LokaleId + "'";
+            con.query(query, function (err, data)
+            {
+                if (data.length)
+                {
+                    query = "SELECT * FROM booking.reservation WHERE (";
+                    query += "(FraDato<'" + fra + "' AND TilDato>'" + fra + "') OR ";
+                    query += "(FraDato<'" + til + "' AND TilDato>'" + til + "') OR ";
+                    query += "(FraDato='" + fra + "' AND TilDato='" + til + "') OR ";
+                    query += "(FraDato>'" + fra + "' AND TilDato<'" + til + "')) AND ";
+                    query += "LokaleId='" + LokaleId+"'";
+                    con.query(query, function (err, data) {
+                        // check om lokalet er booket
+                        if (data.length > 0) {
+                            data.unshift({ 'Booking Error': 1 });
+                            res.end(JSON.stringify(data, null, '\t') + "\n");
+                        }
+                        else
+                        {
+                            // book lokalet
+                            query = "INSERT INTO booking.reservation (LokaleId,FraDato,TilDato,Ansvarlig) VALUES('" + LokaleId + "','" + fra + "','" + til + "','" + Ansvarlig + "') ";
+                            con.query(query, function (err, data) {
+                                res.end('[{"Booking Ok!"}]');
+                            });
+                        }
+                    });
+                } else
+                    res.end('[{"Room not found!"}]');
             });
-
             
         } else
-            res.end("POST error!");
+            res.end('[{"Booking Error!"}]');
 }
 
 var server = http.createServer(function (req, res)
 {
-    // setup trigger hvis der bliver posted til serveren
-    var data="";
-    req.on('data', function (incomingData) { data += incomingData;});
-    req.on('end', function () { if (data!="") getPostData=data.split("&");});
-    sqlCon.checkForDb(req, res, testData, true, handleRequest);
+    path = url.parse(req.url, true);
+    serverPath = "." + path.pathname;
+    var query = path.query;
+
+    // check om key er sat ved brug af api
+    if (query["key"] || serverPath == "./" || serverPath=="./index.html")
+    {
+        if (query["key"] == validKeyCode || serverPath == "./" || serverPath == "./index.html")
+        {
+            // setup trigger hvis der bliver posted til serveren
+            var data = "";
+            req.on('data', function (incomingData) { data += incomingData; });
+            req.on('end', function () { if (data != "") getPostData = data.split("&"); });
+
+            sqlCon.checkForDb(req, res, testData, true, handleRequest); //  Brug/Opret database og test data 
+        }
+        else { res.end('[{"invalid key code!(Use key=123)"}]');} 
+    }
+    else
+        res.end('[{"invalid key code!(Use key=123)"}]');
 });
 server.listen(8080);
